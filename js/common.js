ca00cc86f8ad5d90adfd3100941ab78d98038b93
tqbdev <tranquocbao3897@gmail.com>
2018-04-25 00:30:40 +0700

    Add id to btn and js code to handle

--- js/common.js
+++ js/common.js
@@ -4,6 +4,10 @@
 $(document).ready(function() {
   // BEIN ADD CAM-SV
   // thay đổi label của modal khi click vào tab Đăng Nhập
+  $('#btnLogin').click(function() {
+    $("#modalLogin").modal('show');
+  });
+
   $("#tabLogIn").click(function() {
     $("#myModalLabel").text("Đăng nhập");
   });
@@ -15,6 +19,10 @@ $(document).ready(function() {
 
   // END ADD CAM-SV
   // BEGIN ADD BAO-TQ
+  $('#btnAdvanceSearch').click(function() {
+    $("#modalAdvanceSearch").modal('show');
+  });
+
   $("#refMyAccount").click(function() {
     $("#isChangePassword").prop("checked", false);
     if ($("#isChangePassword").checked) {
@@ -22,6 +30,8 @@ $(document).ready(function() {
     } else {
       $("#areaChangePassword").hide();
     }
+
+    $("#modalInformation").modal('show');
   })
 
   $("#isChangePassword").change(function() {
