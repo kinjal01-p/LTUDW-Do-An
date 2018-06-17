3246ddd958602b5574e1dea7f65717127f10a47a
tqbdev <tranquocbao3897@gmail.com>
2018-06-17 18:36:20 +0700

    Index page hbs

---
+++ public/js/admin_funcs.js
@@ -0,0 +1,48 @@
+// BEGIN ADD BAO-TQ
+"use strict";
+
+$(document).ready(function() {
+  $('#btnPlaceOrder').click(function () {
+    $("#modalPlaceOrder").modal('show');
+  });
+
+  $('#btnAddItem').click(function () {
+    $("#modalAddItem").modal('show');
+  });
+
+  $('#btnEditOrder').click(function () {
+    $("#modalEditOrder").modal('show');
+  });
+
+  $('#btnEditItem').click(function () {
+    $("#modalEditItem").modal('show');
+  });
+
+  $('#btnEditItem2').click(function () {
+    $("#modalEditItem").modal('show');
+  });
+
+  $("#dashboard").show();
+  $("#itemManager").hide();
+  $("#orderManager").hide();
+
+  $("#linkDashboard").click(function() {
+    $("#dashboard").show();
+    $("#itemManager").hide();
+    $("#orderManager").hide();
+  });
+
+  $("#linkItemManager").click(function() {
+    $("#dashboard").hide();
+    $("#itemManager").show();
+    $("#orderManager").hide();
+  });
+
+  $("#linkOrderManager").click(function() {
+    $("#dashboard").hide();
+    $("#itemManager").hide();
+    $("#orderManager").show();
+  });
+});
+
+// END ADD BAO-TQ
