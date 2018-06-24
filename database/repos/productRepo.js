64c06368b9e5b33a4974e4f5d11cdaf121ac5ccd
mthan <thanhanmoc97@gmail.com>
2018-06-24 22:23:39 +0700

    add search normal/advanded

--- database/repos/productRepo.js
+++ database/repos/productRepo.js
@@ -54,3 +54,61 @@ exports.getSameManufacturer = manufacturerId => {
       var sql = `select * from product where id_manufacturer = ${manufacturerId} ORDER BY RAND() limit ${config.appConfig.PRODUCTS_SAME_TYPE}`;
       return db.load(sql);
 }
+
+exports.search = (name, offset) => {
+
+      var sql = `SELECT DISTINCT pro.* , manu.name as manufacturer_name, au.name as author_name
+          FROM product as pro join manufacturer as manu on pro.id_manufacturer = manu.id_manufacturer 
+          join author as au on pro.id_author = au.id_author 
+           where `;
+
+      for (let i = 0; i < name.length; ++i) {
+            sql += `pro.name like '%${name[i]}%' or manu.name like '%${name[i]}%' or au.name like '%${name[i]}%'`;
+            sql += (i >= name.length - 1) ? `` : ` or `;
+      }
+
+      sql += ` limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset}`;
+
+      return db.load(sql);
+}
+exports.searchAll = (name) => {
+
+      var sql = `SELECT DISTINCT pro.* , manu.name as manufacturer_name, au.name as author_name
+          FROM product as pro join manufacturer as manu on pro.id_manufacturer = manu.id_manufacturer 
+          join author as au on pro.id_author = au.id_author where `;
+
+      for (let i = 0; i < name.length; ++i) {
+            sql += `pro.name like '%${name[i]}%' or manu.name like '%${name[i]}%' or au.name like '%${name[i]}%'`;
+            sql += (i >= name.length - 1) ? `` : ` or `;
+      }
+
+      return db.load(sql);
+}
+
+exports.searchAdvanded_All = (data) => {
+
+      var sql = `SELECT DISTINCT pro.* , manu.name as manufacturer_name, au.name as author_name
+          FROM product as pro join manufacturer as manu on pro.id_manufacturer = manu.id_manufacturer 
+          join author as au on pro.id_author = au.id_author join class_product as class on pro.id_class = class.id_class 
+           where pro.name like '%${data.title}%'
+           and manu.name like '%${data.manufacturer}%'
+           and au.name like '%${data.author}%'
+           and class.name like '%${data.class}%'
+           `;
+
+      return db.load(sql);
+}
+
+exports.searchAdvanded = (data, offset) => {
+
+      var sql = `SELECT DISTINCT pro.* , manu.name as manufacturer_name, au.name as author_name
+          FROM product as pro join manufacturer as manu on pro.id_manufacturer = manu.id_manufacturer 
+          join author as au on pro.id_author = au.id_author join class_product as class on pro.id_class = class.id_class 
+           where pro.name like '%${data.title}%'
+           and manu.name like '%${data.manufacturer}%'
+           and au.name like '%${data.author}%'
+           and class.name like '%${data.class}%'
+             limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset}`;
+
+      return db.load(sql);
+}
