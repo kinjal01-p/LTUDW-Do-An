d8485d78407902020de6bbfbb678bf88de55c7d7
tqbdev <tranquocbao3897@gmail.com>
2018-06-27 16:11:24 +0700

    Check out of stock

--- database/repos/productRepo.js
+++ database/repos/productRepo.js
@@ -2,7 +2,7 @@ var db = require('../db.js');
 var config = require('../../config/config.js');
 
 exports.loadAll = offset => {
-      var sql = `select * from product limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset * config.appConfig.PRODUCTS_PER_PAGE}`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset * config.appConfig.PRODUCTS_PER_PAGE}`;
       return db.load(sql);
 }
 
@@ -12,7 +12,7 @@ exports.countAllProducts = () => {
 }
 
 exports.loadAllByType = (typeId, offset) => {
-      var sql = `select * from product where id_class = ${typeId} limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset * config.appConfig.PRODUCTS_PER_PAGE}`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product where id_class = ${typeId} limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset * config.appConfig.PRODUCTS_PER_PAGE}`;
       return db.load(sql);
 }
 
@@ -22,7 +22,7 @@ exports.countByType = typeId => {
 }
 
 exports.loadAllByManufacturer = (manufacturerId, offset) => {
-      var sql = `select * from product where id_manufacturer = ${manufacturerId} limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset * config.appConfig.PRODUCTS_PER_PAGE}`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product where id_manufacturer = ${manufacturerId} limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset * config.appConfig.PRODUCTS_PER_PAGE}`;
       return db.load(sql);
 }
 
@@ -32,27 +32,27 @@ exports.countByManufacturer = manufacturerId => {
 }
 
 exports.single = proId => {
-      var sql = `select * from product where id_product = ${proId}`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product where id_product = ${proId}`;
       return db.load(sql);
 }
 
 exports.top_new = () => {
-      var sql = `select * from product ORDER BY publish_date DESC limit ${config.appConfig.PRODUCTS_PER_TOP} offset 0`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product ORDER BY publish_date DESC limit ${config.appConfig.PRODUCTS_PER_TOP} offset 0`;
       return db.load(sql);
 }
 
 exports.top_sale = () => {
-      var sql = `select * from product ORDER BY sell_amount DESC limit ${config.appConfig.PRODUCTS_PER_TOP} offset 0`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product ORDER BY sell_amount DESC limit ${config.appConfig.PRODUCTS_PER_TOP} offset 0`;
       return db.load(sql);
 }
 
 exports.top_viewed = () => {
-      var sql = `select * from product ORDER BY view_count DESC limit ${config.appConfig.PRODUCTS_PER_TOP} offset 0`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product ORDER BY view_count DESC limit ${config.appConfig.PRODUCTS_PER_TOP} offset 0`;
       return db.load(sql);
 }
 
 exports.details = proId => {
-      var sql = `SELECT pro.*, manu.name as manufacturer_name, au.name as author_name, type.name as type_name
+      var sql = `SELECT pro.*, (pro.in_stock <= 0) as out_of_stock, manu.name as manufacturer_name, au.name as author_name, type.name as type_name
           FROM product as pro join manufacturer as manu on pro.id_manufacturer = manu.id_manufacturer 
           join author as au on pro.id_author = au.id_author
           join class_product as type on pro.id_class = type.id_class
@@ -62,22 +62,22 @@ exports.details = proId => {
 }
 
 exports.getSamples = () => {
-      var sql = `select * from product ORDER BY RAND() limit ${config.appConfig.PRODUCTS_SAMPLE}`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product ORDER BY RAND() limit ${config.appConfig.PRODUCTS_SAMPLE}`;
       return db.load(sql);
 }
 
 exports.getSameTypes = typeId => {
-      var sql = `select * from product where id_class = ${typeId} ORDER BY RAND() limit ${config.appConfig.PRODUCTS_SAME_TYPE}`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product where id_class = ${typeId} ORDER BY RAND() limit ${config.appConfig.PRODUCTS_SAME_TYPE}`;
       return db.load(sql);
 }
 
 exports.getSameManufacturer = manufacturerId => {
-      var sql = `select * from product where id_manufacturer = ${manufacturerId} ORDER BY RAND() limit ${config.appConfig.PRODUCTS_SAME_TYPE}`;
+      var sql = `select *, (in_stock <= 0) as out_of_stock from product where id_manufacturer = ${manufacturerId} ORDER BY RAND() limit ${config.appConfig.PRODUCTS_SAME_TYPE}`;
       return db.load(sql);
 }
 
 exports.search = (name, offset) => {
-      var sql = `SELECT DISTINCT * FROM product where name like '%${name}%' limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset * config.appConfig.PRODUCTS_PER_PAGE}`;
+      var sql = `SELECT *, (in_stock <= 0) as out_of_stock FROM product where name like '%${name}%' limit ${config.appConfig.PRODUCTS_PER_PAGE} offset ${offset * config.appConfig.PRODUCTS_PER_PAGE}`;
 
       return db.load(sql);
 }
@@ -98,7 +98,7 @@ exports.countAllSearchAdvanded = (data) => {
 }
 
 exports.searchAdvanded = (data, offset) => {
-      var sql = `SELECT *
+      var sql = `SELECT *, (in_stock <= 0) as out_of_stock
       FROM product
       where name like '%${data.title}%'
       and id_manufacturer like '%${data.id_manufacturer}%'
@@ -109,7 +109,7 @@ exports.searchAdvanded = (data, offset) => {
 }
 
 exports.loadAllByPrice = (minPrice, maxPrice, offset) => {
-      var sql = `SELECT *
+      var sql = `SELECT *, (in_stock <= 0) as out_of_stock
       FROM product
       where ${minPrice} <= price
       and price < ${maxPrice}
