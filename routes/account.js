cf86c02988148cc1ed7968a71ceefcebcfc24e38
tqbdev <tranquocbao3897@gmail.com>
2018-06-27 14:21:36 +0700

    Almost edit user

--- routes/account.js
+++ routes/account.js
@@ -45,11 +45,11 @@ router.post('/login', (req, res) => {
                   req.session.user = rows[0];
                   req.session.cart = [];
 
-                  res.redirect(req.headers.referer);
+                  res.redirect('/');
             } else {
                   res.render('account_login', {isError: true, email: req.body.email});
             }
-      })
+      });
 });
 
 router.post('/logout', (req, res) => {
@@ -59,8 +59,43 @@ router.post('/logout', (req, res) => {
       res.redirect(req.headers.referer);
 });
 
+router.post('/edit', retrict, (req, res) => {
+      if (req.body.changePassword !== undefined) {
+            var user = {
+                  email: req.body.email,
+                  password: SHA256(req.body.oldpassword + req.body.email).toString()
+            };
+
+            accountRepo.login(user).then(rows => {
+                  if (rows.length <= 0) {
+                        res.render('account_profile', {newError: false, oldError: true});
+                        return;
+                  } else {
+                        if (req.body.newpassword != req.body.repassword) {
+                              res.render('account_profile', {newError: true, oldError: false});
+                              return;
+                        } else {
+                              user.password = SHA256(req.body.newpassword + req.body.email).toString();
+                              accountRepo.changePassword(user).then();
+                        }
+                  }
+            });
+      }
+
+      // var user = {
+      //       password: SHA256(req.body.password + req.body.email).toString(),
+      //       name: req.body.name,
+      //       email: req.body.email,
+      //       address: req.body.address,
+      //       phone: req.body.phone,
+      //       dob: moment(req.body.dob, 'YYYY-MM-DD').format('YYYY-MM-DDTHH:mm')
+      // };
+
+      res.redirect(req.headers.referer);
+});
+
 router.get('/profile', retrict, (req, res) => {
-      res.render('account_profile');
+      res.render('account_profile', {newError: false, oldError: false});
 });
 
 module.exports = router;
