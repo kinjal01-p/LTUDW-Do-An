ec78b7330248a299bdc374c219d91908d30e1b77
tqbdev <tranquocbao3897@gmail.com>
2018-06-27 20:26:48 +0700

    Complete history page

--- database/repos/orderRepo.js
+++ database/repos/orderRepo.js
@@ -1,14 +1,12 @@
 var db = require('../db.js');
 
-exports.cart = idorder => {
+exports.loadByClient = email => {
+    var sql = `select * from store.order where email = '${email}' order by time_place`;
+    return db.load(sql);
+}
 
-
-    var sql = `select p.id_product, p.name, p.price, m.name as manufacturer, s.name as author, o.amount
-from (((select * from store.product_order  where id_product_order = ${idorder}) as o
-join store.product as p on p.id_product = o.id_product) join store.manufacturer as m
-on p.id_manufacturer = m.id_manufacturer) join store.author as s on p.id_author = s.id_author
-`;
-
+exports.loadItemsOrder = id_order => {
+    var sql = `select * from store.product_order where id_order = '${id_order}'`;
     return db.load(sql);
 }
 
