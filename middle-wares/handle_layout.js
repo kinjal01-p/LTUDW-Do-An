a7e398c7c630b2598367fbbdc6f61bc065f51c38
vancam038 <1512038@student.hcmus.edu.vn>
2018-06-28 21:57:36 +0700

    Complete captcha

--- middle-wares/handle_layout.js
+++ middle-wares/handle_layout.js
@@ -1,5 +1,9 @@
 var typeRepo = require('../database/repos/typeRepo.js');
 var manufacturerRepo = require('../database/repos/manufacturerRepo.js');
+var config = require('../config/config.js');
+
+const Recaptcha = require('express-recaptcha').Recaptcha
+const recaptcha = new Recaptcha(config.captchaConfig.RECAPTCHA_SITE_KEY, config.captchaConfig.RECAPTCHA_SECRET_KEY);
 
 module.exports = (req, res, next) => {
       if (req.session.isLogged === undefined) {
@@ -20,7 +24,8 @@ module.exports = (req, res, next) => {
                   manufacturer: values[1],
                   isLogged: req.session.isLogged,
                   curUser: req.session.user,
-                  countCart: totalInCart
+                  countCart: totalInCart,
+                  captcha: recaptcha.render()
             };
 
             next();
